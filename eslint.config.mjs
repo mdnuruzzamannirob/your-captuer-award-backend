b9cc98b809bf360843c1dbc8210a17b313eb683c
sahin <sahinsiraj360@gmail.com>
2026-05-14 14:37:02 +0600

    update tem details info

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,6 @@
 import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
-
-
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
